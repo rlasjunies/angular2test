55dc0fb7baeb1b80061b82b42e35d037abb520eb
Richard Lasjunies <rlasjunies@gmail.com>
2015-10-18 08:06:12 +0200

    Second app, with server typescript compilation, npm ref ...

---
+++ src/app/app.ts
@@ -0,0 +1,9 @@
+/// <reference path='../../node_modules/angular2/angular2.d.ts' />
+
+import {Component, bootstrap} from 'angular2/angular2';
+@Component({
+	selector: 'my-app',
+	template: '<h1>My Second Angular 2 App</h1>'
+})
+class AppComponent { }
+bootstrap(AppComponent);
